440edc19c85760b834dd73b4bf2ebdd9df82431f
feliperod0519 <feliperod0519@hotmail.com>
2019-11-15 16:33:09 -0500

    hello

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ProductListComponent } from '../app/product-list/product-list.component';
+import { ProductDetailComponent } from '../app/product-detail/product-detail.component';
+
+import { CommonModule } from '@angular/common';
+
+const routes: Routes = [
+                        {path: 'products' , component: ProductListComponent},
+                        {path: 'product/:id' , component: ProductDetailComponent},
+                        { path: '',  redirectTo: '/products', pathMatch: 'full' }
+                       ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes), CommonModule],
+  exports: [RouterModule],
+  declarations: []
 })
 export class AppRoutingModule { }
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { InMemoryDbService, InMemoryWebApiModule } from "angular-in-memory-web-api";
+import { DataService } from './data.service';
+
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InMemoryWebApiModule.forRoot(DataService),
+    ProductDetailComponent,
+    ProductListComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
